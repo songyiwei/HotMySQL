7a178f06b6de4b8f99bc440bb5a5a511d3562688
anyuan <anyuan@cloud-feature.(none)>
2015-07-24 00:45:46 +0800

    恢复binlog到临时数据库前重新建立临时数据库

--- lib/init.sh
+++ lib/init.sh
@@ -11,44 +11,57 @@ else
     sed -i "s/expire_logs_days.*/expire_logs_days = 7/" $DBCnf
     sed -i "s/max_binlog_size.*/max_binlog_size = 1024M/" $DBCnf
     sed -i "s/^#binlog_do_db.*/binlog_do_db = $DBName/" $DBCnf
+fi
 
-    #reboot MySQL
-    /etc/init.d/mysql restart 1>/dev/null
-    if [[ $? != 0 ]]; then
-        echo ""
-        echo "Reboot service MySQL faild !"
-        exit 1
-    fi
+#stop MySQL
+/etc/init.d/mysql stop 1>/dev/null
+if [[ $? != 0 ]]; then
+    echo ""
+    echo "Stop service MySQL faild !"
+    exit 1
+fi
+
+cd $DBDataPath
+cp -r $DBName ${DBName}_binlog
+chown -R $DBRunUser:$DBRunUser ${DBName}_binlog
+cd - 1>/dev/null 2>&1
+
+#start MySQL
+/etc/init.d/mysql start 1>/dev/null
+if [[ $? != 0 ]]; then
+    echo ""
+    echo "Start service MySQL faild !"
+    exit 1
 fi
 
 #create database tmp database
-_Result=`ExecSQL "create database ${DBName}_binlog"`
-if [[ $_Result == "Fail" ]]; then
-    echo ""
-    echo "Create database ${DBName}_binlog faild !"
-    exit 1
-fi
-unset _Result
+#_Result=`ExecSQL "create database ${DBName}_binlog"`
+#if [[ $_Result == "Fail" ]]; then
+#    echo ""
+#    echo "Create database ${DBName}_binlog faild !"
+#    exit 1
+#fi
+#unset _Result
 
 #backup database
-_Result=`DumpSQL "$TmpPath/${DBName}_$NowDate.sql" $DBName`
-if [[ $_Result == "Fail" ]]; then
-    echo ""
-    echo "Backup MySQL $DBName faild !"
-    exit 1
-fi
-unset _Result
+#_Result=`DumpSQL "$TmpPath/${DBName}_$NowDate.sql" $DBName`
+#if [[ $_Result == "Fail" ]]; then
+#    echo ""
+#    echo "Backup MySQL $DBName faild !"
+#    exit 1
+#fi
+#unset _Result
 
 #recovery tmp database
-_Result=`ExecSQL "source $TmpPath/${DBName}_$NowDate.sql" ${DBName}_binlog`
-if [[ $_Result == "Fail" ]]; then
-    echo ""
-    echo "Recovery MySQL ${DBName}_binlog faild !"
-    exit 1
-else
-    rm -rf $TmpPath/${DBName}_$NowDate.sql
-fi
-unset _Result
+#_Result=`ExecSQL "source $TmpPath/${DBName}_$NowDate.sql" ${DBName}_binlog`
+#if [[ $_Result == "Fail" ]]; then
+#    echo ""
+#    echo "Recovery MySQL ${DBName}_binlog faild !"
+#    exit 1
+#else
+#    rm -rf $TmpPath/${DBName}_$NowDate.sql
+#fi
+#unset _Result
 
 #build crontab
 echo "$Crontab $BinPath/HotMySQL.sh auto" > $TmpPath/Crontab
